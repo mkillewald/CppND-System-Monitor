3891fbc3b430e7e54d52857edc7a81236c164fe7
Mike Killewald <mkillewald@yahoo.com>
2021-11-05 12:19:51 -0400

    refactoring/clean up

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -3,6 +3,7 @@
 #include <dirent.h>
 #include <unistd.h>
 
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -11,53 +12,58 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-// Returns line from file at given path whose first token matches key_in.
-// If key_in is not supplied, the first line of the file is returned.
+/*
+ * Returns a line from a file at given path whose first token matches key.
+ * If key is not supplied, the first line of the file is returned.
+ * An empty string is returned if key is supplied but not found, or if
+ * the file was not opened.
+ */
 string LinuxParser::GetLineFromFile(const string& path,
                                     const string& key = "") {
   string line;
   std::ifstream filestream(path);
   if (filestream.is_open()) {
+    if (key.empty()) {
+      std::getline(filestream, line);
+      return line;
+    }
     while (std::getline(filestream, line)) {
-      if (key.empty()) {
-        return line;
-      } else if (line.compare(0, key.length(), key) == 0) {
+      if (line.compare(0, key.length(), key) == 0) {
         return line;
       }
     }
   }
-  return line;
+  return string();
 }
 
-// Tokenizes the input string and returns of vector of strings containing the
-// tokens.
+/*
+ * Tokenizes the input string and returns of vector of strings containing the
+ * tokens.
+ */
 vector<string> LinuxParser::GetValuesFromLine(const string& line) {
   string value;
   vector<string> values;
-  if (!line.empty()) {
-    std::istringstream linestream(line);
-    while (linestream >> value) {
-      values.emplace_back(value);
-    }
+  std::istringstream linestream(line);
+  while (linestream >> value) {
+    values.emplace_back(value);
   }
   return values;
 }
 
-// Returns a string value from an input string containing a key value pair
+/*
+ * Returns the string value from an input string containing a white space
+ * delimited key/value pair.
+ */
 string LinuxParser::GetValueFromLine(const string& line) {
   string key, value;
-  if (!line.empty()) {
-    std::istringstream linestream(line);
-    linestream >> key >> value;
-  }
+  std::istringstream linestream(line);
+  linestream >> key >> value;
   return value;
 }
 
 // Read and return the operating system name
 string LinuxParser::OperatingSystem() {
-  string line;
-  string key;
-  string value;
+  string key, value, line;
   std::ifstream filestream(kOSPath);
   if (filestream.is_open()) {
     while (std::getline(filestream, line)) {
@@ -80,7 +86,10 @@ string LinuxParser::OperatingSystem() {
 string LinuxParser::Kernel() {
   string line = GetLineFromFile(kProcDirectory + kVersionFilename);
   vector<string> values = GetValuesFromLine(line);
-  return values.at(KERNEL_INDEX);
+  if (values.size() > KERNEL_INDEX) {
+    return values.at(KERNEL_INDEX);
+  }
+  return string();
 }
 
 // TODO: BONUS: Update this to use std::filesystem
@@ -117,8 +126,8 @@ float LinuxParser::MemoryUtilization() {
       values.push_back(value);
     }
     if (values.size() > MEM_AVAIL_INDEX) {
-      total = stol(values[MEM_TOTAL_INDEX]);
-      available = stol(values[MEM_AVAIL_INDEX]);
+      total = stol(values.at(MEM_TOTAL_INDEX));
+      available = stol(values.at(MEM_AVAIL_INDEX));
       return (float)(total - available) / (float)total;
     }
   }
@@ -129,12 +138,12 @@ float LinuxParser::MemoryUtilization() {
 long LinuxParser::UpTime() {
   string uptime;
   string line = GetLineFromFile(kProcDirectory + kUptimeFilename);
-  if (!line.empty()) {
-    std::istringstream linestream(line);
-    linestream >> uptime;
-    return std::stol(uptime);
+  std::istringstream linestream(line);
+  linestream >> uptime;
+  if (uptime.empty()) {
+    return 0;
   }
-  return 0;
+  return stol(uptime);
 }
 
 // Read and return the number of jiffies for the system
@@ -199,20 +208,20 @@ vector<string> LinuxParser::CpuUtilization() {
 int LinuxParser::TotalProcesses() {
   string line = GetLineFromFile(kProcDirectory + kStatFilename, kProcesses);
   string value = GetValueFromLine(line);
-  if (!value.empty()) {
-    return stoi(value);
+  if (value.empty()) {
+    return 0;
   }
-  return 0;
+  return stoi(value);
 }
 
 // Read and return the number of running processes
 int LinuxParser::RunningProcesses() {
   string line = GetLineFromFile(kProcDirectory + kStatFilename, kProcsRunning);
   string value = GetValueFromLine(line);
-  if (!value.empty()) {
-    return stoi(value);
+  if (value.empty()) {
+    return 0;
   }
-  return 0;
+  return stoi(value);
 }
 
 // Read and return the command associated with a process
@@ -268,6 +277,5 @@ long LinuxParser::UpTime(int pid) {
     long start_time = stol(values.at(PID_STARTTIME_INDEX));
     return UpTime() - (start_time / sysconf(_SC_CLK_TCK));
   }
-
   return 0;
 }

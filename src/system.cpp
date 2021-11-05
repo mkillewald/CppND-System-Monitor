90c372d138f9828a55d515297298e499125c3d88
Mike Killewald <mkillewald@yahoo.com>
2021-11-05 12:30:05 -0400

    made getters const

--- src/system.cpp
+++ src/system.cpp
@@ -30,19 +30,23 @@ vector<Process>& System::Processes() {
 }
 
 // Return the system's kernel identifier (string)
-std::string System::Kernel() { return LinuxParser::Kernel(); }
+std::string System::Kernel() const { return LinuxParser::Kernel(); }
 
 // Return the system's memory utilization
-float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
+float System::MemoryUtilization() const {
+  return LinuxParser::MemoryUtilization();
+}
 
 // Return the operating system name
-std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
+std::string System::OperatingSystem() const {
+  return LinuxParser::OperatingSystem();
+}
 
 // Return the number of processes actively running on the system
-int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
+int System::RunningProcesses() const { return LinuxParser::RunningProcesses(); }
 
 // Return the total number of processes on the system
-int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
+int System::TotalProcesses() const { return LinuxParser::TotalProcesses(); }
 
 // Return the number of seconds since the system started running
-long int System::UpTime() { return LinuxParser::UpTime(); }
+long int System::UpTime() const { return LinuxParser::UpTime(); }

b276bb6c017eeac89ef22d1bdea81be5e8db7ac1
Mike Killewald <mkillewald@yahoo.com>
2021-11-07 10:52:50 -0500

    removed out of date comment and changed cpu width back to 4

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -74,8 +74,6 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
   mvwprintw(window, row, command_column, "COMMAND");
   wattroff(window, COLOR_PAIR(2));
   for (int i = 0; i < n; ++i) {
-    // You need to take care of the fact that the cpu utilization has already
-    // been multiplied by 100.
     //  Clear the line
     mvwprintw(window, ++row, pid_column,
               (string(window->_maxx - 2, ' ').c_str()));
@@ -86,7 +84,7 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
         processes[i].User().substr(0, state_column - user_column - 1).c_str());
     mvwprintw(window, row, state_column, processes[i].State().c_str());
     float cpu = processes[i].CpuUtilization() * 100;
-    mvwprintw(window, row, cpu_column, to_string(cpu).substr(0, 5).c_str());
+    mvwprintw(window, row, cpu_column, to_string(cpu).substr(0, 4).c_str());
     mvwprintw(window, row, ram_column, processes[i].Ram().substr(0, 7).c_str());
     mvwprintw(window, row, time_column,
               Format::ElapsedTime(processes[i].UpTime()).c_str());

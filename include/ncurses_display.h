4006a924fd71686670159601da7934719a73b211
Mike Killewald <mkillewald@yahoo.com>
2021-11-13 13:54:22 -0500

    all string labels now defined as const strings

--- include/ncurses_display.h
+++ include/ncurses_display.h
@@ -9,14 +9,45 @@
 #define SYSTEM_WINDOW_ROWS 6
 
 namespace NCursesDisplay {
+// system info
+const std::string kOs{"Operating System: "};
+const std::string kKernel{"Kernel: "};
+const std::string kUpTime{"Up Time: "};
+const std::string kCpuCore{"CPU"};  // trailing colon ":" will be added
+const std::string kMemory{"Memory:"};
+const std::string kTotal{"Total Processes: "};
+const std::string kRunning{"Running Processes: "};
+const std::string kAlive{"Alive Processes: "};
+
+// processes
+const std::string kPid{"PID"};
+const std::string kUser{"USER"};
+const std::string kState{"S"};
+const std::string kCpu{"CPU%%"};
+const std::string kRam{"RAM[MB]"};
+const std::string kTime{"TIME+"};
+const std::string kCommand{"COMMAND"};
+
+// menu
+const std::string kHideCores{"Hide Cores"};
+const std::string kShowCores{"Show Cores"};
+const std::string kSortOrder{"Sort Order: "};
+const std::string kQuit{"Quit"};
+
 int Keypress(void);
 void CheckEvents(System& system, WINDOW* system_w, WINDOW* process_w, int& n);
 void ClearLine(WINDOW* window, int row);
 void BoldUnderlineAndColor(WINDOW* window, int color, int row, int col,
                            std::string str, size_t pos = 0);
 void AddColorChar(WINDOW* window, int color, chtype c);
-void Resize(WINDOW* system_w, WINDOW* process_w, int& n);
+void Resize(System& system, WINDOW* system_w, WINDOW* process_w, int& n);
 std::string ProgressBar(float percent);
+void SystemMenu(System& system, WINDOW* window, int row, int col);
+void SystemInfo(System& system, WINDOW* window, int row, int col);
+void CpuBars(System& sys, WINDOW* win, int& row, int col);
+void MemoryBar(System& system, WINDOW* window, int row, int col);
+void ProcessMenu(System& system, WINDOW* window, int row, int col);
+void ProcessInfo(System& system, WINDOW* window, int row, int col);
 void DisplaySystem(System& system, WINDOW* window);
 void DisplayProcesses(System& system, WINDOW* window, int n);
 void Display(System& system);
